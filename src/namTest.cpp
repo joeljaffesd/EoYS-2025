fc5568122ae5822fc9e9d325bb559f1b54b6bec3
Joel Jaffe <joel@jaffesd.com>
2025-02-10 11:18:19 -0800

    Functional build with NAM... audio untested.

---
+++ src/namTest.cpp
@@ -0,0 +1,42 @@
+#define NAM_SAMPLE_FLOAT
+#include "al/app/al_DistributedApp.hpp"
+#include "../NeuralAmpModelerCore/NAM/dsp.h"
+
+struct State {
+  int ignore = 0;
+};
+
+class NamTest : public al::DistributedAppWithState<State> {
+private:
+  const char* mPath = "../../resources/MarshallModel.nam";
+  std::unique_ptr<nam::DSP> mModel = nam::get_dsp(mPath);
+
+public:
+  void onInit() override {
+    if (isPrimary()) {
+      mModel->ResetAndPrewarm(this->audioIO().framesPerSecond(), 1);
+    }
+  }
+
+  void onSound(al::AudioIOData& io) override {
+    if (isPrimary()) { // only do audio for primary
+      for (int sample = 0; sample < io.framesPerBuffer(); sample++) {
+        // calculate output from input
+        float input = io.in(0, sample);
+        float output = 0.f;
+        mModel->process(&input, &output, 1);
+
+        // write output to all output channels
+        for (int channel = 0; channel < io.channelsOut(); channel++) {
+          io.out(channel, sample) = output;
+        }
+      }
+    }  
+  }
+};
+
+int main() {
+  NamTest mNamTest;
+  mNamTest.start();
+  return 0;
+}
