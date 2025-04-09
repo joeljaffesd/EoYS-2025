4f965cd5c2044fc207b34b3a625574179cf66626
Lucian <lucianparisi@gmail.com>
2025-04-09 14:37:37 -0700

    Changed `python` to `python3` in `init.sh`

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ git submodule update --init --recursive
 
 # generate .h files for all NAM models
 find resources -name "*.nam" | while read -r file; do
-  python RTNeural/modules/rt-nam/nam_to_header.py "$file" "${file%.nam}.h"
+  python3 RTNeural/modules/rt-nam/nam_to_header.py "$file" "${file%.nam}.h"
 done
