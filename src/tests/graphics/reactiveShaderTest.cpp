e35658de8a51b7427a0ca13d04ce250d4b214371
Joel Jaffe <joel@jaffesd.com>
2025-05-13 16:02:04 -0700

    SphereTest

--- src/tests/graphics/reactiveShaderTest.cpp
+++ src/tests/graphics/reactiveShaderTest.cpp
@@ -16,14 +16,14 @@
 */
 
 struct MyApp : al::DistributedApp {
-    ShaderToSphere shaderSphere;
-    double t = 0.0;
-    SpectralListener specListen;
+  ShaderToSphere shaderSphere;
+  double t = 0.0;
+  SpectralListener specListen;
   DynamicListener dynListen;
-  float flux = 0.01f;
-  float centroid = 1.0f;
-  float rms = 0.0f;
-  float onsetIncrement = 0.0f;
+  al::Parameter flux {"flux", "", 0.01f, 0.f, 1.f};
+  al::Parameter centroid = {"centroid", "", 1.f, 0.f, 20000.f};
+  al::Parameter rms = {"rms", "", 0.f, 0.f, 1.f};
+  al::Parameter onsetIncrement = {"onsetIncrement", "", 0.f, 0.f, 100.f};
   FloatReporter fluxReporter;
   FloatReporter centroidReporter;
   FloatReporter rmsReporter;
@@ -35,73 +35,74 @@ struct MyApp : al::DistributedApp {
 
 
     void onCreate() override {
-        dynListen.setOnsetThresh(0.03);
-        dynListen.setSilenceThresh(0.1);
-        // *** SET SHADER PATH HERE *** //
-        // for shaders folder 1: 
-        shaderSphere.setShaders("../src/shaders/Moving-shaders/moving.vert", "../src/shaders/Reactive-shaders/fractalTest.frag");
-        // for shader folder 2:
-        //shaderSphere.setShaders("../src/shaders/Shader-2/shaderToyDefault.vert", "../src/shaders/Shader-2/shader2.frag");
-        // *** END SET SHADER PATH *** //
-        shaderSphere.setSphere(15.0f, 1000);
+      dynListen.setOnsetThresh(0.03);
+      dynListen.setSilenceThresh(0.1);
+      // *** SET SHADER PATH HERE *** //
+      // for shaders folder 1: 
+      shaderSphere.setShaders("../src/shaders/Moving-shaders/moving.vert", "../src/shaders/Reactive-shaders/fractalTest.frag");
+      // for shader folder 2:
+      //shaderSphere.setShaders("../src/shaders/Shader-2/shaderToyDefault.vert", "../src/shaders/Shader-2/shader2.frag");
+      // *** END SET SHADER PATH *** //
+      shaderSphere.setSphere(15.0f, 1000);
 
-        nav().pos(0, 0, 0);
-        navControl().active(true);
+      nav().pos(0, 0, 0);
+      navControl().active(true);
     }
+
     void onSound(al::AudioIOData &io) override {
-    while (io()) {
-      float in = io.in(0); // mono for now
-      specListen.process(in);
-      dynListen.process(in);
+      if (isPrimary()) {
+        while (io()) {
+        float in = io.in(0); // mono for now
+        specListen.process(in);
+        dynListen.process(in);
 
-      centroidReporter.write(specListen.getCent());
-      fluxReporter.write(specListen.getFlux());
-      rmsReporter.write(dynListen.getRMS());
+        centroidReporter.write(specListen.getCent());
+        fluxReporter.write(specListen.getFlux());
+        rmsReporter.write(dynListen.getRMS());
 
-      //IMPORTANT - since listeners are running process at samplerate, call onset detection in one of the drawing threads - will still be accurate//
-
-    
-
+        //IMPORTANT - since listeners are running process at samplerate, call onset detection in one of the drawing threads - will still be accurate//
+      }
     }
   }
 
 
     void onAnimate(double dt) override {
 
-      if (dynListen.detectOnset() == true){
-      std::cout << "NEW ONSET" << std::endl;
-      onsetIncrement +=0.1;
-      }
-
       t += dt;
     
+      if (isPrimary()) {
+        mOnePoleCent.setCutoff(15000, 60);
+        centroid = mOnePoleCent.lpf(centroidReporter.reportValue());
+        mOnePole.setCutoff(1000, 60);
+        flux = mOnePole.lpf(fluxReporter.reportValue());
+        if (dynListen.detectOnset()) {
+          std::cout << "NEW ONSET" << std::endl;
+          onsetIncrement = onsetIncrement + 0.1f;
+        }
 
-      mOnePoleCent.setCutoff(15000, 60);
-      centroid = mOnePoleCent.lpf(centroidReporter.reportValue());
-      mOnePole.setCutoff(1000, 60);
-      flux = mOnePole.lpf(fluxReporter.reportValue());
+      }
 
     }
 
     void onDraw(al::Graphics& g) override {
-        g.clear(0);
-        shaderSphere.setMatrices(view().viewMatrix(), view().projMatrix(width(), height()));
-        shaderSphere.shadedMesh.shader.use();
+      g.clear(0);
+      shaderSphere.setMatrices(view().viewMatrix(), view().projMatrix(width(), height()));
+      shaderSphere.shadedMesh.shader.use();
 
 
-        shaderSphere.setUniformFloat("u_time", t);
-        shaderSphere.setUniformFloat("onset", onsetIncrement);
-        shaderSphere.setUniformFloat("cent", centroid);
-        shaderSphere.setUniformFloat("flux", flux);
-        std::cout << "flux: " << flux << "cent: " << centroid << std::endl;
-      
+      shaderSphere.setUniformFloat("u_time", t);
+      shaderSphere.setUniformFloat("onset", onsetIncrement);
+      shaderSphere.setUniformFloat("cent", centroid);
+      shaderSphere.setUniformFloat("flux", flux);
+      // std::cout << "flux: " << flux << "cent: " << centroid << std::endl;
+    
 
 
-        shaderSphere.draw(g);
+      shaderSphere.draw(g);
     }
 };
 
 int main() {
-    MyApp app;
-    app.start();
+  MyApp app;
+  app.start();
 }
