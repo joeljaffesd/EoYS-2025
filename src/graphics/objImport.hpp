19d1ea2f133f5f60dc46a0693bb6b933e939e77e
Colin <colindunnester@gmail.com>
2025-05-13 16:53:09 -0700

    guess i missed that file

--- src/graphics/objImport.hpp
+++ src/graphics/objImport.hpp
@@ -1,11 +1,8 @@
-
-//#include "al/core.hpp"
 #include "al/app/al_App.hpp"
 #include "al_ext/assets3d/al_Asset.hpp"
-//#include "module/img/loadImage.hpp"
-#include <algorithm>  // max
-#include <cstdint>    // uint8_t
+#include "al/graphics/al_Image.hpp"
 #include <vector>
+#include <algorithm>
 
 using namespace al;
 using namespace std;
@@ -13,58 +10,81 @@ using namespace std;
 struct AssetEngine {
   Scene *ascene{nullptr};
   Vec3f scene_min, scene_max, scene_center;
-  //Texture tex;
+  Texture tex;
   vector<Mesh> meshes;
-  float a = 0.f, b = 0.f, c = 0.f;  // current rotation angle
+  float a = 0.f, b = 0.f, c = 0.f;
   ParameterBool assetShow{"assetShow", "", true};
   Parameter scale{"scale", "", 1.f, 0.f, 10.f};
 
   void loadAssets() {
-    std::string fileName = "../assets/rough-human-ouroboros.obj";
+    //std::string fileName = "../assets/3dModels/sphereEye.obj";
+    std::string fileName = "../assets/3dModels/daEyeLegacyExport.obj";
+    // std::string fileName = "../assets/3dModels/skel-size-test-10.obj";
     ascene = Scene::import(fileName);
     if (!ascene) {
       printf("error reading %s\n", fileName.c_str());
       return;
     }
+
     ascene->getBounds(scene_min, scene_max);
     scene_center = (scene_min + scene_max) / 2.f;
     ascene->print();
 
-    // extract meshes from scene
+    // extract meshes
     meshes.resize(ascene->meshes());
-    for (int i = 0; i < ascene->meshes(); i += 1) {
+    for (int i = 0; i < ascene->meshes(); ++i) {
       ascene->mesh(i, meshes[i]);
+    }
+
+    // load texture
+    Image img;
+    if (img.load("../assets/3dModels/bakeshiz.png")) {
+      // tex.create2D(img.width(), img.height(), GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE);
+      // tex.submit(img.array().data(), GL_RGBA, GL_UNSIGNED_BYTE);
+      // tex.filterMin(Texture::LINEAR_MIPMAP_LINEAR);
+      // tex.filterMag(Texture::LINEAR);
+      // tex.generateMipmap();
+
+      tex.create2D(img.width(), img.height());
+      tex.filter(Texture::LINEAR);
+      tex.submit(img.array().data(), GL_RGBA, GL_UNSIGNED_BYTE);
+      
+    } else {
+      printf("Failed to load texture: bakeshiz.png\n");
     }
   }
 
   void draw(Graphics &g) {
     if (!assetShow) return;
+
     gl::depthTesting(true);
     g.lighting(true);
     g.pushMatrix();
 
-    // rotate it around the y axis
+    // animate rotation
     g.rotate(a, b, c, 0.f);
     a -= 0.2f;
     b += 0.2f;
     c += 0.2f;
 
-    // scale the whole asset to fit into our view frustum
-    float tmp = scene_max[0] - scene_min[0];
-    tmp = std::max(scene_max[1] - scene_min[1], tmp);
-    tmp = std::max(scene_max[2] - scene_min[2], tmp);
+    // center and scale the model
+    float tmp = std::max({scene_max[0] - scene_min[0], scene_max[1] - scene_min[1], scene_max[2] - scene_min[2]});
     tmp = 2.f / tmp;
     g.scale(tmp);
     g.scale(scale);
-
-    // center the model
     g.translate(-scene_center);
-    
-    // draw all the meshes in the scene
+
+    // bind and draw with texture
+    tex.bind(0);
+    g.texture();  // enables texture usage
+
     for (auto &m : meshes) {
       g.draw(m);
     }
-    
+
+    tex.unbind(0);
+    // g.noTexture();  // ❌ Removed: not defined in al::Graphics
+
     g.popMatrix();
     g.lighting(false);
   }
