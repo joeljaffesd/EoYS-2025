5234bc85db5093f5d4adddde76bb2b06c1755906
Joel Jaffe <joel@jaffesd.com>
2025-05-16 11:02:41 -0700

    Added Tim's fix for `fractalTest.frag`

--- src/shaders/Reactive-shaders/fractalTest.frag
+++ src/shaders/Reactive-shaders/fractalTest.frag
@@ -1,6 +1,8 @@
 #version 330 core
 
 in vec3 vPos; //recieve from vert
+in vec2 vUV;
+
 out vec4 fragColor;
 
 uniform float u_time;
@@ -12,16 +14,24 @@ uniform float flux;
 // *** STARTER CODE INSPIRED BY : https://www.shadertoy.com/view/4lSSRy *** //
 
 void main() {
-    vec2 uv = 0.275 * vPos.xy;
-    float t = (u_time * (0.001*(flux*3 )))+onset; //+ (cent);
+    vec2 uv = 0.03 * vPos.xy;
+    // float t = (u_time * (0.001*(flux*3 )))+onset; //+ (cent);
+    float t = (u_time * 0.01); //XXX simplify back to whats in shadertoy example
+
     //t += flux;
     //t +=cent;
     float k = cos(t);
     float l = sin(t);
     float s = 0.2 + (onset / u_time);
 
+    // for(int i=0; i<64; ++i) {
+        // uv  = abs(uv) - s*flux;//-onset;    // Mirror
+        // uv *= mat2(k,-l,l,k); // Rotate
+        // s  *= .95156;///(t+1);         // Scale
+    // }
+    // XXX simplify back to shadertoy example
     for(int i=0; i<64; ++i) {
-        uv  = abs(uv) - s*flux;//-onset;    // Mirror
+        uv  = abs(uv) - s;//-onset;    // Mirror
         uv *= mat2(k,-l,l,k); // Rotate
         s  *= .95156;///(t+1);         // Scale
     }
@@ -32,7 +42,7 @@ void main() {
 
     //fragColor =  vec4(vec3(x),1);
      //fragColor =  x* vec4(1,2*flux,3,1);
-   
+    //fragColor = vec4(vec3(x), 1);
     fragColor = .5 + .5*cos(6.28318*(40.0*length(uv))*vec4(-1,2,3+flux,1));
 
 }
--- src/tests/graphics/reactiveShaderTest.cpp
+++ src/tests/graphics/reactiveShaderTest.cpp
@@ -9,35 +9,27 @@
 #include "src/graphics/vfxMain.hpp"
 #include <iostream>
 
-/* TO DO :
-* move var memory writing outside of audio callback - buffer approach?
-* move printing and writing to draw
-*
-*/
-
 struct MyApp : al::DistributedApp {
   ShaderToSphere shaderSphere;
-  double t = 0.0;
   SpectralListener specListen;
   DynamicListener dynListen;
+  
+  al::Parameter now {"now", "", 0.f, 0.f, std::numeric_limits<float>::max()};
   al::Parameter flux {"flux", "", 0.01f, 0.f, 1.f};
   al::Parameter centroid = {"centroid", "", 1.f, 0.f, 20000.f};
   al::Parameter rms = {"rms", "", 0.f, 0.f, 1.f};
   al::Parameter onsetIncrement = {"onsetIncrement", "", 0.f, 0.f, 100.f};
+  al::ParameterBundle mParams {"Uniforms"};
+
   FloatReporter fluxReporter;
   FloatReporter centroidReporter;
   FloatReporter rmsReporter;
-  //BoolReporter onsetReporter;
-
-
   giml::OnePole<float> mOnePole;
   giml::OnePole<float> mOnePoleCent;
 
   void onInit() override {
-    this->parameterServer().registerParameter(flux);
-    this->parameterServer().registerParameter(centroid);
-    this->parameterServer().registerParameter(rms);
-    this->parameterServer().registerParameter(onsetIncrement);
+    mParams << now << flux << centroid << rms << onsetIncrement;
+    this->parameterServer().registerParameterBundle(mParams);
   }
 
 
@@ -52,9 +44,6 @@ struct MyApp : al::DistributedApp {
     //shaderSphere.setShaders("../src/shaders/Shader-2/shaderToyDefault.vert", "../src/shaders/Shader-2/shader2.frag");
     // *** END SET SHADER PATH *** //
     shaderSphere.setSphere(15.0f, 1000);
-
-    nav().pos(0, 0, 0);
-    navControl().active(true);
   }
 
   void onSound(al::AudioIOData &io) override {
@@ -68,17 +57,17 @@ struct MyApp : al::DistributedApp {
         fluxReporter.write(specListen.getFlux());
         rmsReporter.write(dynListen.getRMS());
 
-        //IMPORTANT - since listeners are running process at samplerate, call onset detection in one of the drawing threads - will still be accurate//
+        //IMPORTANT - since listeners are running process at samplerate, 
+        // call onset detection in one of the drawing threads - will still be accurate//
       }
     }
   }
 
 
   void onAnimate(double dt) override {
-
-    t += dt;
-  
     if (isPrimary()) {
+
+      now = now + float(dt);
 
       mOnePoleCent.setCutoff(15000, 60);
       centroid = mOnePoleCent.lpf(centroidReporter.reportValue());
@@ -92,14 +81,13 @@ struct MyApp : al::DistributedApp {
       }
 
     }
-
   }
 
   void onDraw(al::Graphics& g) override {
     g.clear(0);
     g.shader(shaderSphere.shadedMesh.shader);
 
-    shaderSphere.setUniformFloat("u_time", t);
+    shaderSphere.setUniformFloat("u_time", now);
     shaderSphere.setUniformFloat("onset", onsetIncrement);
     shaderSphere.setUniformFloat("cent", centroid);
     shaderSphere.setUniformFloat("flux", flux);
